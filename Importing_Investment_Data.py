9f5f0133bb5a423452e64ad130faabc469ce94d3
Thomas <thomas.vanrooyen22@gmail.com>
2022-07-15 12:57:08 +0200

    added some comments, installing rest of packages

--- Importing_Investment_Data.py
+++ Importing_Investment_Data.py
@@ -1,3 +1,21 @@
+################################################
+# This Project is heavily based off of Derek
+# Banas' "Investing with Data Science" video
+# changes on his demo will include:
+# 1: Pulling investment data from south 
+# african markets 
+# 2: Extra functionality that may include 
+# analysis of crypto, bonds and other securities
+################################################
+
+# Installing various packages
+# pip install numpy
+# pip install pandas 
+# pip install matplotlib.pyplot
+# pip install matplotlib.dates
+
+
+
 # Importing numpy
 import numpy as np
 
